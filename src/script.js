1739ec873b3986b916b3ac1aa65cfcee99460ee4
TheoTr <71769486+TheoTr@users.noreply.github.com>
2023-10-19 12:10:09 +0200

    Add Date to index page

--- src/script.js
+++ src/script.js
@@ -11,10 +11,27 @@ function afficherHeure() {
 
     const heureActuelle = heureFormat + ':' + minutesFormat + ':' + secondesFormat;
     divHeure.textContent = heureActuelle;
+
 }
+function afficherDate() {
+    const divDate = document.getElementById('date');
+    const date = new Date();
+    const jour = date.getDate();
+    const mois = date.getMonth() + 1;
+    const annee = date.getFullYear();
+
+    const jourFormat = jour < 10 ? '0' + jour : jour;
+    const moisFormat = mois < 10 ? '0' + mois : mois;
+
+    const dateActuelle = jourFormat + '/' + moisFormat + '/' + annee;
+    divDate.textContent = dateActuelle;
+}
+
 
 // Mettre à jour l'heure toutes les secondes
 setInterval(afficherHeure, 1000);
 
 // Appeler la fonction initiale pour afficher l'heure immédiatement
 afficherHeure();
+afficherDate();
+
